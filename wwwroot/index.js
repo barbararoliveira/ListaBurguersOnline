927fe61b35f6492b75f8c3ea4be627e210f06d01
Bárbara Oliveira <babyr.oliveira@gmail.com>
2020-11-22 01:55:43 -0300

    Corrige erro.

--- wwwroot/index.js
+++ wwwroot/index.js
@@ -1,8 +1,5 @@
 const iniciar = async () => {
-    const mensagem = document.getElementById('mensagem');
-    const response = await fetch('/quemsoueuapi');
-    const result = await response.json();
-    mensagem.innerHTML = `Meu nome é ${result.mensagem}. Me siga no GitHub: <a href="${result.url}">${result.mensagem1}</a>.`;
+    
 };
 
 document.addEventListener('DOMContentLoaded', iniciar);
